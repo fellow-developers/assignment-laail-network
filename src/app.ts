a351f8f293ce8daae7d65eb4642caad5afecae48
satyam-seth <satyam1998.1998@gmail.com>
2022-12-30 22:54:07 +0530

    fix `req.body` `undefined` error

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,11 @@ import web from './routes/web';
 const app = express();
 const port = 3000;
 
-// Routes
-app.use("/api", web);
-
 // JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+
+// Routes
+app.use("/api", web);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
