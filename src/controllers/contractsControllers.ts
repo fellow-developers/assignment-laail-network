65faf36b9fcf3ea6ae389003c296cc3ca9e75121
satyam-seth <satyam1998.1998@gmail.com>
2022-12-31 14:12:58 +0530

    fix all ts typing errors

--- src/controllers/contractsControllers.ts
+++ src/controllers/contractsControllers.ts
@@ -1,13 +1,13 @@
+import { Request, Response } from 'express';
+
 export const getContracts = (req: Request, res: Response) => {
     console.log("Get Contracts");
 
-    // @ts-ignore
     const n = req.params.n;
 
     // TODO: Add logic to retrieve contracts from db depending on n and send it back 
     console.log(n);
 
-    // @ts-ignore
     res.json(`Contracts: ${n}`);
 }
 
@@ -15,12 +15,10 @@ export const getContracts = (req: Request, res: Response) => {
 export const createContracts = (req: Request, res: Response) => {
     console.log("Creating Contracts");
 
-    // @ts-ignore
     const data = req.body;
     console.log(data);
 
     // TODO: Add logic to create contracts in db and send it back
 
-    // @ts-ignore
     res.json('Contracts Created');
 }
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,16 +1,12 @@
-import express from "express";
+import { Router } from "express";
 import { getUser, createUser } from "../controllers/usersControllers";
 import { getContracts, createContracts } from "../controllers/contractsControllers";
 
-const router = express.Router();
+const router = Router();
 
-// @ts-ignore
 router.get("/users", getUser);
-// @ts-ignore
 router.post("/users", createUser);
-// @ts-ignore
 router.get("/contracts/:n", getContracts)
-// @ts-ignore
 router.post("/contracts", createContracts);
 
 export default router;
