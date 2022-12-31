f37bbc85f11042d329e5e673d8c355da9c125bbf
satyam-seth <satyam1998.1998@gmail.com>
2022-12-31 23:41:40 +0530

    store contract details in db

--- src/controllers/contractsControllers.ts
+++ src/controllers/contractsControllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { ContractModel, ContractStatus } from '../models/contracts';
+import { UserModel } from '../models/users';
 // import { ContractModel } from '../models/contracts';
 
 export const getContracts = (req: Request, res: Response) => {
@@ -15,13 +17,41 @@ export const getContracts = (req: Request, res: Response) => {
 }
 
 
-export const createContracts = (req: Request, res: Response) => {
+export const createContracts = async (req: Request, res: Response) => {
     console.log("Creating Contracts");
 
-    const data = req.body;
-    console.log(data);
+    console.log("Request Body -> ", req.body);
+    const { lenderId, borrowerId, principle, interest } = req.body;
+
 
     // TODO: Add logic to create contracts in db and send it back
 
-    res.json('Contracts Created');
+    try {
+        // Retrieve lender and borrower from db
+        const lender = await UserModel.findById({ _id: lenderId });
+        const borrower = await UserModel.findById({ _id: borrowerId });
+
+        console.log('Lender -> ', lender);
+        console.log('Borrower -> ', borrower);
+
+        // Save contract in the db
+        const doc = new ContractModel({
+            lender: lender,
+            borrower: borrower,
+            principle: principle,
+            interest: interest,
+            status: ContractStatus.Active,
+        });
+
+
+        const result = await doc.save();
+        console.log('Data Store in Db -> ', result);
+
+        res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+
+        // TODO: Pass only the validation errors and not the whole error object and also handle 500 error
+        res.status(400).json(error);
+    }
 }
